668ef1c97d30a42400c93deff5bea52b310475ab
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-15 02:52:35 +0700

    JS-DOM and Update Markup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const priceInput = document.getElementById('coffee-price');
-const quantity = document.getElementById('coffee-quantity');
-const confirmBtn = document.getElementById('confirm-order');
+const priceInput = document.getElementById('product-price');
+const quantityInput = document.getElementById('product-quantity');
+const shippingInput = document.getElementById('product-shipping');
+
+const totalBtn = document.getElementById('total-price');
 const resultDiv = document.getElementById('result');
 const errorBox = document.getElementById('error');
 
@@ -9,7 +11,44 @@ const parseInputs = (...input) => {
     return input.map((str) => parseInt(str));
 };
 
-// FN- : Hide Error
+// FN-2 : Validate
+const isValidInputs = (...input) => {
+    return input.every((num) => typeof num === 'number' && !isNaN(num));
+};
+
+// FN-3: Hide Error
 const hideErrors = () => {
     errorBox.classList.add('invisible');
 };
+hideErrors();
+
+// FN-4 : CreateOrder
+function createOrder() {
+    hideErrors();
+    const inputs = [priceInput.value, quantityInput.value, shippingInput.value];
+    const parsedInputs = parseInputs(...inputs);
+
+    if (isValidInputs(...parsedInputs)) {
+        const [price, quantity, shipping] = parsedInputs;
+        resultDiv.innerText = price * quantity + shipping;
+    } else {
+        resultDiv.innerText = '';
+        handleAdditionError(inputs, parsedInputs);
+    }
+}
+// Binding
+totalBtn.addEventListener('click', createOrder);
+
+// FN-5 : HandleError
+function handleAdditionError(inputs, numbers) {
+    const fullMessage = inputs.reduce((message, str, index) => {
+        if (isValidInputs(numbers[index])) {
+            return message + '';
+        } else {
+            return message + `${str} is not a number. `;
+        }
+    }, 'Please enter three valid numbers! ');
+
+    errorBox.classList.remove('invisible');
+    errorBox.innerText = fullMessage;
+}
